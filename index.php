e58f056497852983754fd7d6cffa5cdd40ed74cf
3041TAN-14 <3041TAN-14@boronkay.local>
2023-11-27 08:58:16 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>PHP gyakorlat</title>
+</head>
+<body>
+    <h1>PHP gyakorlat</h1>
+    <nav>
+        <ul>
+            <li><a href="postoffice.php">Irányítószám kereső<a></li>
+            <li><a href="counties.php">Megyék<a></li>    
+            <li><a href="cities.php">Városok<a></li>    
+        <ul>
+    </nav>
+</body>
+</html>
